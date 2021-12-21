cc97ae5166afec1b71bd711dbc327492686ffd58
Kuchkarov Samandar <samandar>
2021-12-21 11:01:03 +0500

    add

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
+
+
+
 const prizes = [
-    {
-      text: "Бесплатный интернет на год",
-      color: "#E5474C",
-      font: "#fff",
-    },
-    { 
-      text: "Бесплатный интернет на пол года",
-      color: "#fff",
-      font: "#E5474C",
-    },
-    { 
-      text: "Бесплатный интернет на 3 месяца",
-      color: "#E5474C",
-      font: "#fff",
-    },
     {
       text: "500 000",
       color: "#fff",
       font: "#E5474C",
     },
-    {
+    { 
       text: "380 000",
       color: "#E5474C",
       font: "#fff",
     },
-    {
+    { 
       text: "100 000",
       color: "#fff",
       font: "#E5474C",
@@ -40,23 +28,36 @@ const prizes = [
       font: "#E5474C",
     },
     {
-      text: "Роутер",
+      text: "Роутера Mercusis ac1200",
       color: "#E5474C",
       font: "#fff",
     },
     {
-      text: "Amediateka",
+      text: "ТВ приставки TX-6",
       color: "#fff",
       font: "#E5474C",
     },
+    {
+      text: "Промо-коды на кинотеатр Амедиатека",
+      color: "#E5474C",
+      font: "#fff",
+      
+    },
     
-  ];
+];
   
   // создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
   const wheel = document.querySelector(".deal-wheel");
   const spinner = wheel.querySelector(".spinner");
   const trigger = wheel.querySelector(".btn-spin");
   const ticker = wheel.querySelector(".ticker");
+  let isPlayed = false
+  let user = {
+    login:'',
+    tel:''
+  }
+  let aviablePrizes = []
+  let order = 1
   
   // на сколько секторов нарезаем круг
   const prizeSlice = 360 / prizes.length;
@@ -146,7 +147,7 @@ const prizes = [
       // убираем анимацию язычка
       ticker.style.animation = "none";
       // и через 10 миллисекунд отменяем это, чтобы он вернулся в первоначальное положение
-      setTimeout(() => ticker.style.animation = null, 10);
+      setTimeout(() => ticker.style.animation = null, 20);
       // после того, как язычок прошёл сектор - делаем его текущим 
       currentSlice = slice;
     }
@@ -155,26 +156,22 @@ const prizes = [
   };
   
   const prizeChose = (name) =>{
-    if(name==='Бесплатный интернет на год'){
+    if(name===prizes[0].text){
         return  Math.floor(360*6+20)
-    }else if(name==='Amediateka'){
-        return  Math.floor(360*6+347)
-    }else if(name==='Роутер'){
-        return  Math.floor(360*6+307)
-    }else if(name==='20 000'){
-        return  Math.floor(360*6+270)
-    }else if(name==='50 000'){
-        return  Math.floor(360*6+230)
-    }else if(name==='100 000'){
-        return  Math.floor(360*6+200)
-    }else if(name==='380 000'){
-        return  Math.floor(360*6+160)
-    }else if(name==='500 000'){
-        return  Math.floor(360*6+130)
-    }else if(name==='Бесплатный интернет на 3 месяца'){
-        return  Math.floor(360*6+90)
-    }else if(name==='Бесплатный интернет на пол года'){
-        return  Math.floor(360*6+60)
+    }else if(name===prizes[1].text){
+      return  Math.floor(360*6+65)
+    }else if(name===prizes[2].text){
+      return  Math.floor(360*6+110)
+    }else if(name===prizes[3].text){
+      return  Math.floor(360*6+155)
+    }else if(name===prizes[4].text){
+      return  Math.floor(360*6+200)
+    }else if(name===prizes[5].text){
+      return  Math.floor(360*6+245)
+    }else if(name===prizes[6].text){
+      return  Math.floor(360*6+290)
+    }else if(name===prizes[7].text){
+      return  Math.floor(360*6+335)
     }
     
   }
@@ -184,14 +181,41 @@ const prizes = [
   };
   
   // отслеживаем нажатие на кнопку
-  trigger.addEventListener("click", () => {
+  trigger.addEventListener("click", async() => {
     
+    if( isPlayed === true){
+      return
+    }else{
+      isPlayed = true
+      trigger.disabled = true;
+    }
+    let superPrizes = aviablePrizes.filter(item=>item.name!=='Промо-коды на кинотеатр Амедиатека')
+    console.log(order)
+     if(order%2!==0||superPrizes.length==0){
+      let amedia = aviablePrizes.filter(item=>item.name === 'Промо-коды на кинотеатр Амедиатека')
+      rotation = prizeChose(prizes[7].text);
+      let promo = await getPromo()
+      addOrder(prizes[7].text)
+      setTimeout(() => {  
+        alert(`Промокод "${promo.name}".Активировать промокод на Амедиатеку вы можете на сайте play.tvcom.uz, в личном кабинете. После активации промокода, выберите тариф «Amediateka» и активируйте его.`)
+      }, 8000);
+      
+      removeDevices(amedia[0].name,amedia[0].count)
 
-
-    trigger.disabled = true;
-    rotation = prizeChose(prizes[1].text);
-
-
+    }else{
+      const randomIndex = Math.floor(Math.random() * superPrizes.length);
+      const randomItem = superPrizes[randomIndex]
+      addOrder(randomItem.name)
+      if(randomItem.name.includes('000')){
+        alert(`${randomItem.name}.Бонусные единицы зачисляемые на лицевой счет нельзя вывести в денежном эквиваленте, 1 бонусная единица равна 1суму.`)
+      }else if(randomItem.name==='Роутера Mercusis ac1200'){
+        alert(`Wi-Fi роутер вы можете забрать у нас в офисе c 03.01.2022 до 30.01.2022 с 09:00 до 18:00, по адресу ул. Шахрисабз, д.10б (ориентир м. Айбек). При себе необходимо иметь документ удостоверяющий личность.`)
+      }else if(randomItem.name==='ТВ приставки TX-6'){
+        alert('ТВ-приставку вы можете забрать у нас в офисе c 03.01.2022 до 30.01.2022 с 09:00 до 18:00, по адресу ул. Шахрисабз, д.10б (ориентир м. Айбек). При себе необходимо иметь документ удостоверяющий личность.')
+      } 
+      rotation = prizeChose(randomItem.name);
+      removeDevices(randomItem.name,randomItem.count)
+    }
 
 
     prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
@@ -207,9 +231,7 @@ const prizes = [
   
   // отслеживаем, когда закончилась анимация вращения колеса
   spinner.addEventListener("transitionend", () => {
-    // останавливаем отрисовку вращения
     cancelAnimationFrame(tickerAnim);
-    // получаем текущее значение поворота колеса
     rotation %= 360;
     // выбираем приз
     selectPrize();
@@ -227,6 +249,155 @@ const prizes = [
  let btnSub =  document.querySelector('.submit')
 
  btnSub.addEventListener('click',()=>{
-   document.querySelector('.form').style.display = 'none'
-   document.querySelector('.deal-wheel').style.display = 'grid'
+   const textareas = document.querySelectorAll('input')
+   if(textareas[0].value&&textareas[1].value){
+     axios({
+      method: 'POST',
+      url:`http://play.tvcom.uz:777/api/blacked/get/checker`,
+      }).then(async (e)=>{
+        if(!isPlayed){
+          user.login = textareas[0].value
+          user.tel = textareas[1].value
+          order = e.data.message.length+2;
+          document.querySelector('.form').style.display = 'none'
+          document.querySelector('.deal-wheel').style.display = 'grid'
+          document.querySelector('body').classList.add('back2')
+          aviablePrizes =  await getDevices()
+        }
+
+        
+      }).catch((e)=>{
+         console.log(e)
+      }) 
+   }else{
+    console.log(2)
+   }
+   
  })
+
+  function getDevices(){
+    return axios({
+         method: 'POST',
+         url:`http://play.tvcom.uz:777/api/blacked/get/device`,
+           }).then((e)=>{
+           let aviablePrizes = e.data.message.filter(item=>item.count!==0)
+           console.log(e)
+           return aviablePrizes
+          }).catch((e)=>{
+              console.log(e)
+            })
+  }
+  function addOrder(podarok){
+    return axios({
+      method: 'POST',
+      url:`http://play.tvcom.uz:777/api/blacked/set/checker`,
+      params:{
+        login:user.login,
+        podarok,
+        number:user.tel
+      }
+       }).then((e)=>{
+        console.log(e,'add order')
+     }).catch((e)=>{
+         console.log(e)
+       })
+  }
+  function removeDevices(name,count){
+    return axios({
+         method: 'POST',
+         url:`http://play.tvcom.uz:777/api/blacked/set/device`,
+         params:{
+           name,
+           count:count-1
+         },
+          }).then((e)=>{
+          console.log(e)
+          return aviablePrizes
+        }).catch((e)=>{
+            console.log(e)
+          })
+  }
+  function getPromo(){
+    return axios({
+         method: 'POST',
+         url:`http://play.tvcom.uz:777/api/blacked/get/amedia`,
+          }).then((e)=>{
+           removePromo(e.data.message[0].name)
+          return e.data.message[0]
+        }).catch((e)=>{
+            console.log(e)
+          })
+  }
+  function removePromo(name){
+    return axios({
+         method: 'POST',
+         url:`http://play.tvcom.uz:777/api/blacked/remove/amedia`,
+         params:{
+           name
+         }
+          }).then((e)=>{
+         
+          return e.data.message[0]
+        }).catch((e)=>{
+            console.log(e)
+          })
+  }
+
+  
+
+//  axios({
+//   method: 'POST',
+//   url:`http://play.tvcom.uz:777/api/blacked/get/device`,
+//     }).then((e)=>{
+//     let aviablePrizes = e.data.message.filter(item=>item.count!==0)
+//     let superPrizes = aviablePrizes.filter(item=>item.name !== 'Промо-коды на кинотеатр Амедиатека')
+//     if(superPrizes!==0||superPrizes.length===0){
+//       axios({
+//         method: 'POST',
+//         url:`http://play.tvcom.uz:777/api/blacked/get/amedia`,
+//           }).then((e)=>{
+//             let promo = e.data.message[0]
+//             rotation = prizeChose(prizes[7]);
+//             setTimeout(() => {
+//               alert(`Промокод "${promo}".Активировать промокод на Амедиатеку вы можете на сайте play.tvcom.uz, в личном кабинете. После активации промокода, выберите тариф «Amediateka» и активируйте его.`)
+//             }, 4000);
+           
+//             rotation = prizeChose(prizes[7].text);
+//             console.log(rotation)
+//             // axios({
+//             //   method: 'POST',
+//             //   params:{
+//             //     name:promo,
+//             //   },
+//             //   url:`http://play.tvcom.uz:777/api/blacked/set/amedia`,
+//             //     }).then((e)=>{
+                  
+//             //   }).catch((e)=>{
+//             //     console.log(e)
+//             //   }) 
+//         }).catch((e)=>{
+//             console.log(e)
+//         })
+
+
+//     }else{
+//       const randomIndex = Math.floor(Math.random() * superPrizes.length);
+//       const randomItem = superPrizes[randomIndex]
+//       rotation = prizeChose(randomItem);
+//       axios({
+//         method: 'POST',
+//         params:{
+//           name:randomItem.text,
+//           count:randomItem.count-1,
+//         },
+//         url:`http://play.tvcom.uz:777/api/blacked/set/device`,
+//           }).then((e)=>{
+//             console.log(e)
+//         }).catch((e)=>{
+//           console.log(e)
+//         })}
+
+//   }).catch((e)=>{
+//      console.log(e)
+//   }) 
+
